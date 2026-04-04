3d5286f398c5aec1981eeefbebb31999e136cb58
Venyige Balázs <venyigebalazs96@gmail.com>
2026-04-04 22:37:33 +0200

    ci: build workflow added

--- app/Jobs/AwsQueueTest.php
+++ app/Jobs/AwsQueueTest.php
@@ -6,10 +6,16 @@ use App\Models\JobDependency;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class AwsQueueTest implements ShouldQueue
 {
     use Queueable;
+
+    /**
+     * @var int[]
+     */
+    public array $backoff = [1, 5, 10];
 
     protected JobDependency $jobDependency;
 
@@ -27,5 +33,16 @@ class AwsQueueTest implements ShouldQueue
     public function handle(): void
     {
         Log::info('AWS Queue Test Job executed successfully. Number from job_dependency: '.$this->jobDependency->some_number);
+    }
+
+    /**
+     * Handle a job failure.
+     */
+    public function failed(?Throwable $exception): void
+    {
+        Log::error('AWS Queue Test Job failed.', [
+            'job_dependency_id' => $this->jobDependency->id,
+            'error' => $exception?->getMessage(),
+        ]);
     }
 }
